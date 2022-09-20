d7ddddc27b4000919437713572fde8616a9d3f9d
Incredibletochi <victor.oraekwuotu@gmail.com>
2022-09-20 15:00:32 +0000

    Task 102

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def magic_calculation(a, b, c):
+    """Match bytecode provided by Holberton School."""
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
