2c93f2c9f3b80f216f1803ae3a33ac86593382bc
incredibletochi <victor.oraekwuotu@gmail.com>
2023-01-16 11:21:16 +0000

    Hot fix

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get request to the URL and display the body of the response
-curl -sLX PUT -d "user_id=98" -H "Origin: You got me!" 0.0.0.0:5000/catch_me
+# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
